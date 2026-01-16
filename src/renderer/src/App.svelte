a6af8e23ef65c499035ccd65ee50889c5667fe01
kortecx <jayachandra.s.reddy@gmail.com>
2026-01-16 14:14:07 +0530

    It all started here
    Executable intelligence platform

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import kortecxLogo from './assets/kortecx.png';
+
+</script>
+
+<img alt="logo" class="logo" src={kortecxLogo} />
+<div class="creator">Kortecx - Executable Intelligence Platform</div>
+<div class="text">
+  Unlimited AI possibilities with exceptional performance powered by
+  <span class="svelte">KortecxDB</span>
+  and
+  <span class="ts">KortecxCLI</span>
+</div>
